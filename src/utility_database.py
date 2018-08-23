603dbb47d97a841c7a759925cdeedaa6f852a286
Satoshi Miyazawa <koitaroh@gmail.com>
2018-08-23 19:14:01 +0900

    Support only Postgresql

--- src/utility_database.py
+++ src/utility_database.py
@@ -1,15 +1,8 @@
-import os
-import pandas
-import collections
-import smart_open
-import random
-import multiprocessing
 import sqlalchemy
-import pymysql
 import configparser
+import sshtunnel
 
-
-# Logging ver. 2016-07-12
+# Logging ver. 2017-10-30
 from logging import handlers
 import logging
 logger = logging.getLogger(__name__)
@@ -17,8 +10,8 @@ logger.setLevel(logging.DEBUG)
 fh = logging.handlers.RotatingFileHandler('log.log', maxBytes=1000000, backupCount=3)  # file handler
 fh.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()  # console handler
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(lineno)d - [%(levelname)s][%(funcName)s] - %(message)s')
 ch.setFormatter(formatter)
 fh.setFormatter(formatter)
 logger.addHandler(fh)
@@ -29,37 +22,61 @@ logger.info('Initializing %s', __name__)
 # Database configuration
 conf = configparser.ConfigParser()
 conf.read('config.cfg')
-LOCAL_DB_POSTGRESQL = {
-    "host": conf.get('local_postgresql', 'host'),
-    "user": conf.get('local_postgresql', 'user'),
-    "passwd": conf.get('local_postgresql', 'passwd'),
-    "db_name": conf.get('local_postgresql', 'db_name'),
+
+
+SSH_456 = {
+    "host": conf.get('ssh_456', 'host'),
+    "user": conf.get('ssh_456', 'user'),
+    "key_path": conf.get('ssh_456', 'key_path'),
 }
 
-LOCAL_DB_MYSQL = {
-    "host": conf.get('local_mysql', 'host'),
-    "user": conf.get('local_mysql', 'user'),
-    "passwd": conf.get('local_mysql', 'passwd'),
-    "db_name": conf.get('local_mysql', 'db_name'),
+DB_456_GEOTWEET = {
+    "host": conf.get('456_geotweet', 'host'),
+    "user": conf.get('456_geotweet', 'user'),
+    "passwd": conf.get('456_geotweet', 'passwd'),
+    "db_name": conf.get('456_geotweet', 'db_name'),
 }
-
-ENGINE_CONF = "postgresql://" + LOCAL_DB_POSTGRESQL["user"] + ":" + LOCAL_DB_POSTGRESQL["passwd"] + "@" + LOCAL_DB_POSTGRESQL["host"] + ":5432/" + LOCAL_DB_POSTGRESQL["db_name"]
-ENGINE_CONF_LOCAL_MYSQL = "mysql+pymysql://" + LOCAL_DB_MYSQL["user"] + ":" + LOCAL_DB_MYSQL["passwd"] + "@" + LOCAL_DB_MYSQL["host"] + "/" + LOCAL_DB_MYSQL["db_name"] + "?charset=utf8mb4"
 
 
 def establish_db_connection_postgresql_geotweet():
-    # logger.info('Establishing engine.')
+    server = sshtunnel.SSHTunnelForwarder(
+        (SSH_456['host'], 22),
+        ssh_username = SSH_456['user'],
+        ssh_pkey= SSH_456['key_path'],
+        # ssh_password = "<password>",
+        remote_bind_address=('localhost', 5432))
+        # local_bind_address = ('localhost', 53749))
+    server.start() #start ssh sever
+    logger.info('Server connected via SSH')
+
+    #connect to PostgreSQL
+    local_port = str(server.local_bind_port)
+    # print(local_port)
+    ENGINE_CONF = "postgresql://" + DB_456_GEOTWEET["user"] + ":" + DB_456_GEOTWEET["passwd"] + "@" + DB_456_GEOTWEET["host"] + ":" + local_port + "/" + DB_456_GEOTWEET["db_name"]
     engine = sqlalchemy.create_engine(ENGINE_CONF)
     conn = engine.connect()
     metadata = sqlalchemy.MetaData(engine)
     return engine, conn, metadata
 
 
-def establish_db_connection_mysql_local():
-    engine = sqlalchemy.create_engine(ENGINE_CONF_LOCAL_MYSQL)
-    conn = engine.connect()
-    metadata = sqlalchemy.MetaData(engine)
-    return engine, conn, metadata
+
+# ENGINE_CONF = "postgresql://" + LOCAL_DB_POSTGRESQL["user"] + ":" + LOCAL_DB_POSTGRESQL["passwd"] + "@" + LOCAL_DB_POSTGRESQL["host"] + ":5432/" + LOCAL_DB_POSTGRESQL["db_name"]
+# ENGINE_CONF_LOCAL_MYSQL = "mysql+pymysql://" + LOCAL_DB_MYSQL["user"] + ":" + LOCAL_DB_MYSQL["passwd"] + "@" + LOCAL_DB_MYSQL["host"] + "/" + LOCAL_DB_MYSQL["db_name"] + "?charset=utf8mb4"
+#
+#
+# def establish_db_connection_postgresql_geotweet():
+#     # logger.info('Establishing engine.')
+#     engine = sqlalchemy.create_engine(ENGINE_CONF)
+#     conn = engine.connect()
+#     metadata = sqlalchemy.MetaData(engine)
+#     return engine, conn, metadata
+#
+#
+# def establish_db_connection_mysql_local():
+#     engine = sqlalchemy.create_engine(ENGINE_CONF_LOCAL_MYSQL)
+#     conn = engine.connect()
+#     metadata = sqlalchemy.MetaData(engine)
+#     return engine, conn, metadata
 
 #
 # def establish_db_connection_postgresql_inbounds_with_ssh():
@@ -109,4 +126,4 @@ def establish_db_connection_mysql_local():
 
 
 if __name__ == '__main__':
-    engine, conn, metadata = establish_db_connection_postgresql_geotweet(ENGINE_CONF)
+    engine, conn, metadata = establish_db_connection_postgresql_geotweet()
